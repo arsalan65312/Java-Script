c15bc91207a2349cc69830121b72661b3eb22bb0
Muhammad Arsalan <snapmail89@gmail.com>
2025-05-16 22:34:10 +0500

    commit

--- lecture_07/lec7.js
+++ lecture_07/lec7.js
@@ -15,6 +15,31 @@ const roundOff=2.9999999
 console.log(Math.floor(roundOff))
 console.log(Math.floor(2.99999999999999999999999999))
 console.log(Math.floor(-2.5))
+console.log(Math.floor(-9.5777777777777777777777777))
+
+
+const ceilFunction=2.0000000001
+console.log(Math.ceil(ceilFunction));
+console.log(Math.ceil(-23.1));
+
+
+console.log(Math.round(2.50));
+console.log(Math.round(2.49));
+
+
+console.log(Math.floor((Math.random()*10)+10));
+console.log(Math.floor(Math.random()*(20-10)+10));
+
+
+
+console.log(8/0);
+console.log(0/0);
+
+console.log(typeof Infinity);
+console.log(typeof NaN);
+
+
+// let age = Number(prompt("Enter your age")); // it runs only in browser not on nodejs
 
 
 
