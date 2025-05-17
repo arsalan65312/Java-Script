9a74a3b1e3caced301947f2181558146d9e82f98
Muhammad Arsalan <snapmail89@gmail.com>
2025-05-17 12:38:31 +0500

    commit

---
+++ lecture_08-Truthy-and-Falsy/lec8.js
@@ -0,0 +1,14 @@
+console.log(Boolean(Infinity)); // true
+console.log(Boolean(NaN)); // false
+console.log(Boolean("")); // false
+console.log(Boolean(" ")); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(null)); // false
+
+
+
+
+let number = 12;
+console.log(Boolean(number)); // true
+
---
+++ lecture_09-Comparison-Operators/lec9.js
@@ -0,0 +1,35 @@
+const age=34;
+console.log(age>=18)
+
+const numb1=22
+const numb2='24'
+console.log(numb1>=numb2);
+
+const password='2343'
+console.log((password.length)>=8)
+
+
+console.log(23=='23'); // true
+console.log(23==='23'); // false
+console.log(23!=='23'); // true
+console.log(23!='23'); // false
+
+console.log(23>23<23); // true
+console.log(true>23); // false
+
+console.log(false==0); // true
+console.log(true==1); // true
+
+let number='232323'
+console.log(typeof(+number));
+
+
+
+
+
+
+
+
+
+
+
